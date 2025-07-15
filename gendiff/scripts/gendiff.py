496d61c4c16bd7a27995516570f150bc3ff2b455
maltush <maltushw@gmail.com>
2025-07-15 09:00:56 +0300

    Добавлены file1.json, file2.json и парсер

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
+
+def read_json(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -16,6 +21,9 @@ def main():
     )
     
     args = parser.parse_args()
+
+    data1 = read_json(args.first_file)
+    data2 = read_json(args.second_file)
 
     print(f'Первый файл: {args.first_file}')
     print(f'Второй файл: {args.second_file}')
