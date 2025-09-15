8e16c34d023f2ba16046a7a2a2df429f880d4124
maltush <maltushw@gmail.com>
2025-09-15 12:13:13 +0300

    Попробуем переименовать  test

---
+++ tests/test_10_diff.py
@@ -0,0 +1,34 @@
+import pytest
+
+from gendiff.scripts.generate_diff import generate_diff
+from gendiff.scripts.parser import read_file
+
+
+@pytest.mark.parametrize('file_path1, file_path2, expected_result', [
+    ('tests/test_data/file1.json', 'tests/test_data/file2.json', 'tests/test_data/expected_result_json.txt'),
+    ('tests/test_data/file1.yaml', 'tests/test_data/file2.yaml', 'tests/test_data/expected_result_yaml.txt')
+])
+def test_generate_diff(file_path1, file_path2, expected_result):
+    diff = generate_diff(file_path1, file_path2)
+    expected = read_file(expected_result).strip()
+    assert diff.strip() == expected
+
+
+@pytest.mark.parametrize('file_path1, file_path2, expected_result', [
+    ('tests/test_data/file1.json', 'tests/test_data/file2.json', 'tests/test_data/expected_result_plain.txt'),
+    ('tests/test_data/file1.yaml', 'tests/test_data/file2.yaml', 'tests/test_data/expected_result_plain.txt')
+])
+def test_generate_diff_plain(file_path1, file_path2, expected_result):
+    diff = generate_diff(file_path1, file_path2, formatter="plain")
+    expected = read_file(expected_result).strip()
+    assert diff.strip() == expected
+
+
+@pytest.mark.parametrize('file_path1, file_path2, expected_result', [
+    ('tests/test_data/file1.json', 'tests/test_data/file2.json', 'tests/test_data/expected_result_json_format.txt'),
+    ('tests/test_data/file1.yaml', 'tests/test_data/file2.yaml', 'tests/test_data/expected_result_json_format.txt')
+])
+def test_generate_diff_json(file_path1, file_path2, expected_result):
+    diff = generate_diff(file_path1, file_path2, formatter="json")
+    expected = read_file(expected_result).strip()
+    assert diff.strip() == expected
