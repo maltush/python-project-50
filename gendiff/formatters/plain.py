f894c074d3ec4c75c97255fd97264976fd071bf4
maltush <maltushw@gmail.com>
2025-10-02 12:03:14 +0300

    исправлен make_plain_item

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -24,8 +24,7 @@ def make_plain_item(item, path=''):
     messages = {
         'added': f"Property '{current_path}' was added with value: {new_value}",
         'deleted': f"Property '{current_path}' was removed",
-        'modified': f"""Property '{current_path}' was updated. 
-        From {old_value} to {new_value}"""
+        'modified': f"Property '{current_path}' was updated. From {old_value} to {new_value}"
     }
 
     if action in messages:
