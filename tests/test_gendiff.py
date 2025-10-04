7dcc7791f408af439d94982f8275c9f9bdab357a
maltush <maltushw@gmail.com>
2025-10-04 11:27:54 +0300

    перенос строки

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,9 +5,11 @@ from gendiff.scripts.parser import read_file
 
 
 @pytest.mark.parametrize('file_path1, file_path2, expected_result', [
-    ('tests/test_data/file1.json', 'tests/test_data/file2.json', 'tests/test_data/expected_result_json.txt')
+    ('tests/test_data/file1.json', 'tests/test_data/file2.json',
+      'tests/test_data/expected_result_json.txt')
     ,
-    ('tests/test_data/file1.yaml', 'tests/test_data/file2.yaml', 'tests/test_data/expected_result_yaml.txt')
+    ('tests/test_data/file1.yaml', 'tests/test_data/file2.yaml',
+      'tests/test_data/expected_result_yaml.txt')
 ])
 def test_generate_diff(file_path1, file_path2, expected_result):
     diff = generate_diff(file_path1, file_path2)
@@ -16,9 +18,11 @@ def test_generate_diff(file_path1, file_path2, expected_result):
 
 
 @pytest.mark.parametrize('file_path1, file_path2, expected_result', [
-    ('tests/test_data/file1.json', 'tests/test_data/file2.json', 'tests/test_data/expected_result_plain.txt')
+    ('tests/test_data/file1.json', 'tests/test_data/file2.json',
+      'tests/test_data/expected_result_plain.txt')
     ,
-    ('tests/test_data/file1.yaml', 'tests/test_data/file2.yaml', 'tests/test_data/expected_result_plain.txt')
+    ('tests/test_data/file1.yaml', 'tests/test_data/file2.yaml',
+      'tests/test_data/expected_result_plain.txt')
 ])
 def test_generate_diff_plain(file_path1, file_path2, expected_result):
 
@@ -28,9 +32,11 @@ def test_generate_diff_plain(file_path1, file_path2, expected_result):
     
 
 @pytest.mark.parametrize('file_path1, file_path2, expected_result', [
-    ('tests/test_data/file1.json', 'tests/test_data/file2.json', 'tests/test_data/expected_result_json_format.txt')
+    ('tests/test_data/file1.json', 'tests/test_data/file2.json',
+      'tests/test_data/expected_result_json_format.txt')
     ,
-    ('tests/test_data/file1.yaml', 'tests/test_data/file2.yaml', 'tests/test_data/expected_result_json_format.txt')
+    ('tests/test_data/file1.yaml', 'tests/test_data/file2.yaml',
+      'tests/test_data/expected_result_json_format.txt')
 ])
 def test_generate_diff_json(file_path1, file_path2, expected_result):
     diff = generate_diff(file_path1, file_path2, formatter="json")
